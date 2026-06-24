53b5c21cd92979499386ba26dc06fe3b8c187560
saratyagi24-dot <saratyagi24@gmail.com>
2026-06-24 20:19:09 +0530

    Add band name generator and tip calculator projects

---
+++ project1.py
@@ -0,0 +1,3 @@
+print("Click Run to run the final project.")
+print("Welcome to The Band Name Generator")
+print("Your band name could be "+input("What is the city's name you grew up in?\n ")+ " "+input("What is the name of your pet?\n") )
